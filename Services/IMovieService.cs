f149a231cb36a95e7ef6b7ee06a7e41101dc2e71
Petr Valach <petr.valach@hotmail.com>
2026-02-04 21:16:43 +0100

    a

--- Services/IMovieService.cs
+++ Services/IMovieService.cs
@@ -8,15 +8,8 @@ public interface IMovieService
     Task<List<Movie>> GetMovies(int startIndex, int count);
     Task<int> GetTotalMovieCount();
 
-    // New: only movies that have any future schedule in the database
-    Task<List<Movie>> GetMoviesWithFutureSchedules(int startIndex, int count);
-    Task<int> GetTotalMovieCountWithFutureSchedules();
-
     Task<List<Schedule>> GetTodaysSchedules();
     Task<List<Schedule>> SearchTodaysSchedules(string query);
-
-    // New: schedules grouped by day for a specific movie (upcoming days)
-    Task<Dictionary<DateOnly, List<Schedule>>> GetUpcomingSchedulesForMovie(int movieId, int days);
 
     Task<Dictionary<int, Movie>> GetMoviesByIdsAsync(IEnumerable<int> ids);
     void InvalidatePerformanceCache();
