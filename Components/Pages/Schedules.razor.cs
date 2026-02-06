dd3c1504ad4b92cedc3a596e46c6881214de0dcb
Petr Valach <petr.valach@hotmail.com>
2026-02-06 12:11:12 +0100

    a

--- Components/Pages/Schedules.razor.cs
+++ Components/Pages/Schedules.razor.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using vkine.Models;
